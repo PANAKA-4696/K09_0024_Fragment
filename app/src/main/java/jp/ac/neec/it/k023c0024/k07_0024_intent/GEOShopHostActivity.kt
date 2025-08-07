758edaa2b970d257c58df9cf88cd39d757713e77
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-07 16:05:27 +0900

    Fragmentの変更に伴って使用するための設定変更

---
+++ app/src/main/java/jp/ac/neec/it/k023c0024/k07_0024_intent/GEOShopHostActivity.kt
@@ -0,0 +1,17 @@
+package jp.ac.neec.it.k023c0024.k07_0024_intent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+
+class GEOShopHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_geoshop_host)
+
+        // Fragmentを動的に追加する処理
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, GEOShopActivityFragment())
+        fragmentTransaction.commit()
+    }
+}
