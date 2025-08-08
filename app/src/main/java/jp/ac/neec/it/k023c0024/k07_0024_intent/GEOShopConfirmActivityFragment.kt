cd22e9eeb10f3b3dcfa61f6689794e4ed6fbbada
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-08-08 12:41:27 +0900

    GEOShopConfirmActivityFragment.ktの処理作成

---
+++ app/src/main/java/jp/ac/neec/it/k023c0024/k07_0024_intent/GEOShopConfirmActivityFragment.kt
@@ -0,0 +1,71 @@
+package jp.ac.neec.it.k023c0024.k07_0024_intent
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+class GEOShopConfirmActivityFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // ここでFragmentがメニューを持つことを有効化する
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //Fragmentのレイアウトをここでインフレ―としてViewを生成する
+        val view = inflater.inflate(R.layout.activity_geoshop_confirm, container, false)
+
+        //Bundleからデータを取得
+        val money = arguments?.getInt("money", 0)
+        val price1 = arguments?.getInt("price1", 0)
+        val game1 = arguments?.getString("game1")
+
+        val btGEO = view.findViewById<Button>(R.id.btSubShop1)
+        btGEO.setOnClickListener(ClickBtGEOListinere())
+
+        val puMoney = view.findViewById<TextView>(R.id.puMoney)
+        puMoney.setText(money.toString())
+
+        val GEOmsg = view.findViewById<TextView>(R.id.Shop1Msg)
+        var msg = ""
+
+        if (money != null && price1 != null && money >= price1) {
+            msg = "ご購入ありがとうございます。\n ${game1} ${price1}"
+        }
+        else{
+            msg = "所持金が足りません。"
+        }
+
+        GEOmsg.setText(msg)
+
+        return view
+    }
+
+    private inner class ClickBtGEOListinere : View.OnClickListener{
+        override fun onClick(view: View?) {
+            requireActivity().finish()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //選択されたメニューが「戻る」の場合、アクティビティを終了
+        if (item.itemId == android.R.id.home) {
+            //FragmentManagerのスタックから一つ戻る
+            requireActivity().supportFragmentManager.popBackStack()
+            return true
+        }
+        //親クラスの同名メソッドを呼び出す
+        return super.onOptionsItemSelected(item)
+    }
+
+}
